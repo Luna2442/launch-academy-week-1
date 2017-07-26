0f52470777723f1110f61db66d0f6af81892cc27
Alex Luna <alexluna@Alexs-MacBook-Air.local>
2017-07-26 09:10:53 -0400

    updated symbols

--- blackjack/lib/deck.rb
+++ blackjack/lib/deck.rb
@@ -2,7 +2,7 @@ class Deck
   attr_reader :all_cards, :suits, :values
 
   def initialize
-    @suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+    @suits = ['♦', '♥', '♣', '♠']
     @values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
     @all_cards = []
     @values.each do |value|
